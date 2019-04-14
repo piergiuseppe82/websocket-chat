150448b47fa99e214dbdb3595d8cd60d79e7eea8
piergiuseppe la cava <piergiuseppe@placava-debian.placava-debian>
2019-04-14 14:07:43 +0200

    Quick Fix 3 Pom for Openshift

--- src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
+++ src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
@@ -1,10 +1,16 @@
 package it.piergiuseppe82.examples.websocketchat;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsocketChatApplication {
+public class WebsocketChatApplication extends SpringBootServletInitializer {
+	@Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(WebsocketChatApplication.class);
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketChatApplication.class, args);
