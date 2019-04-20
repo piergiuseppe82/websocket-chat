5e69d55fca137540eb68652fd33fb94da210a76c
piergiuseppe la cava <piergiuseppe@placava-debian.placava-debian>
2019-04-20 14:28:26 +0200

    Clean code

--- src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
+++ src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
@@ -1,26 +1,10 @@
 package it.piergiuseppe82.examples.websocketchat;
 
-/*
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsocketChatApplication /* extends SpringBootServletInitializer */ {
-
-/*************************************************************************************
-Extension of SpringBootServletInitializer and Override
-not required with Embedded Server.
-
-Required for Deployable WAR.
-*************************************************************************************/
-
-	// @Override
-  // protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-  //     return application.sources(WebsocketChatApplication.class);
-  // }
+public class WebsocketChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketChatApplication.class, args);
