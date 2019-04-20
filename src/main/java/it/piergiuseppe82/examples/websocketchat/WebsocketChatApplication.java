3452d8e240e1268d0dff5bc7dca8d7d6ce09868d
piergiuseppe la cava <piergiuseppe@placava-debian.placava-debian>
2019-04-20 10:56:08 +0200

    Wildfly configuration removed

--- src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
+++ src/main/java/it/piergiuseppe82/examples/websocketchat/WebsocketChatApplication.java
@@ -1,12 +1,14 @@
 package it.piergiuseppe82.examples.websocketchat;
 
+/*
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsocketChatApplication extends SpringBootServletInitializer {
+public class WebsocketChatApplication /* extends SpringBootServletInitializer */ {
 
 /*************************************************************************************
 Extension of SpringBootServletInitializer and Override
@@ -15,10 +17,10 @@ not required with Embedded Server.
 Required for Deployable WAR.
 *************************************************************************************/
 
-	@Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(WebsocketChatApplication.class);
-  }
+	// @Override
+  // protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  //     return application.sources(WebsocketChatApplication.class);
+  // }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketChatApplication.class, args);
